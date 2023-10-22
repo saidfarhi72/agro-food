194ddca356d256ce19229bf808b9af7e0940bf76
said farhi <said.farhi72@gmail.com>
2023-10-22 23:17:51 +0100

    add navbar

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from "./components/Navbar"
+
+
+export default function Home() {
+  return (
+    <div data-theme={"light"} className=" h-[100vh] w-full">
+      <Navbar/>
+
+ 
+    </div>
+  )
+}
